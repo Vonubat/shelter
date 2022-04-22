c38e7c7db05c70acfe9cd6c976249d778828d445
Egor Tabunov <Vonubat@gmail.com>
2022-04-22 11:24:21 +0300

    feat: add functionality modal-window

--- shelter/pages/main/js/script.js
+++ shelter/pages/main/js/script.js
@@ -10,9 +10,9 @@ const PETS = [
     description:
       "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
     age: "2 months",
-    inoculations: ["none"],
-    diseases: ["none"],
-    parasites: ["none"],
+    inoculations: "none",
+    diseases: "none",
+    parasites: "none",
   },
   {
     name: "Sophia",
@@ -22,9 +22,9 @@ const PETS = [
     description:
       "Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
     age: "1 month",
-    inoculations: ["parvovirus"],
-    diseases: ["none"],
-    parasites: ["none"],
+    inoculations: "parvovirus",
+    diseases: "none",
+    parasites: "none",
   },
   {
     name: "Woody",
@@ -34,9 +34,9 @@ const PETS = [
     description:
       "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
     age: "3 years 6 months",
-    inoculations: ["adenovirus", "distemper"],
-    diseases: ["right back leg mobility reduced"],
-    parasites: ["none"],
+    inoculations: "adenovirus, distemper",
+    diseases: "right back leg mobility reduced",
+    parasites: "none",
   },
   {
     name: "Scarlett",
@@ -46,9 +46,9 @@ const PETS = [
     description:
       "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
     age: "3 months",
-    inoculations: ["parainfluenza"],
-    diseases: ["none"],
-    parasites: ["none"],
+    inoculations: "parainfluenza",
+    diseases: "none",
+    parasites: "none",
   },
   {
     name: "Katrine",
@@ -58,9 +58,9 @@ const PETS = [
     description:
       "Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.",
     age: "6 months",
-    inoculations: ["panleukopenia"],
-    diseases: ["none"],
-    parasites: ["none"],
+    inoculations: "panleukopenia",
+    diseases: "none",
+    parasites: "none",
   },
   {
     name: "Timmy",
@@ -70,9 +70,9 @@ const PETS = [
     description:
       "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
     age: "2 years 3 months",
-    inoculations: ["calicivirus", "viral rhinotracheitis"],
-    diseases: ["kidney stones"],
-    parasites: ["none"],
+    inoculations: "calicivirus, viral rhinotracheitis",
+    diseases: "kidney stones",
+    parasites: "none",
   },
   {
     name: "Freddie",
@@ -82,9 +82,9 @@ const PETS = [
     description:
       "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
     age: "2 months",
-    inoculations: ["rabies"],
-    diseases: ["none"],
-    parasites: ["none"],
+    inoculations: "rabies",
+    diseases: "none",
+    parasites: "none",
   },
   {
     name: "Charly",
@@ -94,13 +94,14 @@ const PETS = [
     description:
       "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
     age: "8 years",
-    inoculations: ["bordetella bronchiseptica", "leptospirosis"],
-    diseases: ["deafness", "blindness"],
-    parasites: ["lice", "fleas"],
+    inoculations: "bordetella bronchiseptica, leptospirosis",
+    diseases: "deafness, blindness",
+    parasites: "lice, fleas",
   },
 ];
 
 // Burger menu
+
 const BTN_ICON = document.querySelector("#start-screen > div > header > img");
 const BURGER_MENU = document.querySelector("#start-screen > div > nav");
 const BODY_BG = document.querySelector("body");
@@ -169,6 +170,7 @@ const ITEM_LEFT_IMGS = document
 const ITEM_RIGHT_IMGS = document
   .querySelector("#items-right")
   .getElementsByTagName("img");
+let CARDS = document.querySelectorAll(".card");
 
 function randomizeCards(sideNames, sideImgs) {
   let uniqueNumber = 0;
@@ -232,7 +234,88 @@ for (const item of ITEMS) {
     }
     BTN_LEFT.addEventListener("click", moveLeft);
     BTN_RIGHT.addEventListener("click", moveRight);
+
+    /*
+    !!!!!!!! from pop-up (below)
+    because the cards are recreated, it is necessary to update the variables +addEventListener there, after recreation new cards
+     ITEM_ACTIVE.innerHTML = ITEM_LEFT.innerHTML;
+    } else {
+      ITEM_ACTIVE.innerHTML = ITEM_RIGHT.innerHTML;
+    */
+
+    CARDS = document.querySelectorAll(".card");
+    for (const card of CARDS) {
+      card.addEventListener("click", openModalWindow);
+    }
   });
 }
 
 // Popup
+
+const CLOSE_BTN = document.querySelector("#close-btn");
+const MODAL_WINDOW = document.querySelector(
+  "#pets > div > div.modal-window-container"
+);
+
+let modalName = document.querySelector(
+  "#pets > div > div.modal-window-container> div > div > h3"
+);
+let modalImg = document.querySelector(
+  "#pets > div > div.modal-window-container> div > img"
+);
+let modalTypeBreed = document.querySelector(
+  "#pets > div > div.modal-window-container > div > div > h4"
+);
+let modalDescription = document.querySelector(
+  "#pets > div > div.modal-window-container > div > div > h5"
+);
+let modalAge = document.querySelector("#age");
+let modalInoculations = document.querySelector("#inoculations");
+let modalDiseases = document.querySelector("#diseases");
+let modalParasites = document.querySelector("#parasites");
+
+function openModalWindow(event) {
+  let currentCardName = event.currentTarget.querySelector(".name").innerHTML;
+
+  for (const pet of PETS) {
+    if (currentCardName === modalName) {
+      break;
+    } else if (pet.name === currentCardName) {
+      modalName.innerHTML = pet.name;
+      modalImg.src = pet.img;
+      modalTypeBreed.innerHTML = pet.type + " - " + pet.breed;
+      modalDescription.innerHTML = pet.description;
+      modalAge.innerHTML = pet.age;
+      modalInoculations.innerHTML = pet.inoculations;
+      modalDiseases.innerHTML = pet.diseases;
+      modalParasites.innerHTML = pet.parasites;
+    } else {
+      continue;
+    }
+  }
+
+  console.log(event.currentTarget);
+
+  MODAL_WINDOW.classList.toggle("active");
+  BODY_BG.classList.toggle("active");
+
+  if (MODAL_WINDOW.classList.contains("active")) {
+    BODY_BG.style.overflowY = "hidden";
+  } else {
+    BODY_BG.style.overflowY = "";
+  }
+}
+
+function closeModalWindow(event) {
+  if (event.target === BODY_BG || event.target == CLOSE_BTN) {
+    MODAL_WINDOW.classList.remove("active");
+    BODY_BG.classList.remove("active");
+    BODY_BG.style.overflowY = "";
+  }
+}
+
+BODY_BG.addEventListener("click", closeModalWindow);
+// create first assocoation cards with modalWindows
+for (const card of CARDS) {
+  card.addEventListener("click", openModalWindow);
+}
